a7bc0fbfb98307c4fe36e479805d8b3d362e5dd6
jadefred <97032726+jadefred@users.noreply.github.com>
2022-10-02 11:10:42 +0200

    generate y axis scale and draw on canvas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData
 
 const width = 800;
 const height = 400;
-const padding = 30;
+const padding = 40;
 
 const svg = d3.select("svg");
 let xScale;
@@ -17,16 +17,28 @@ const generateScales = (arr) => {
     .scaleLinear()
     .domain([d3.min(arr, (item) => item.Year), d3.max(arr, (item) => item.Year)])
     .range([padding, width - padding]);
+
+  yScale = d3
+    .scaleTime()
+    // .domain([0, d3.max(arr, (item) => item.Time)])
+    .range([height - padding, padding]);
 };
 
 const generateAxis = () => {
   let xAxis = d3.axisBottom(xScale);
+  let yAxis = d3.axisLeft(yScale);
 
   svg
     .append("g")
     .call(xAxis)
     .attr("id", "x-axis")
     .attr("transform", "translate(0, " + (height - padding) + ")");
+
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + padding + ", 0)");
 };
 
 async function fetchData() {
